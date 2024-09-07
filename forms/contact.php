9b8094f5eb01f5a1ee9087f5a826d341e0c25798
Godfrey <rweyemamu088@gmail.com>
2024-09-07 10:52:10 +0300

    SMPT contact from google added

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,43 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+// Load Composer's autoloader
+require 'vendor/autoload.php';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+try {
+    // Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;   // Enable verbose debug output
+    $mail->isSMTP();                         // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                // Enable SMTP authentication
+    $mail->Username   = 'godfreymuganyizi45@gmail.com'; // SMTP username
+    $mail->Password   = 'gimrwpyiizqcnuwc ';   // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable implicit TLS encryption
+    $mail->Port       = 465;                 // TCP port to connect to
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    // Get user input
+    $recipient = $_POST['recipient'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    // Recipients
+    $mail->setFrom('godfreymuganyizi45@gmail.com', 'kyomah');
+    $mail->addAddress($recipient);           // Add a recipient
+    $mail->addReplyTo('rweyemamu088@gmail.com', 'Information');
 
-  echo $contact->send();
+    // Content
+    $mail->isHTML(true);                     // Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $message;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
